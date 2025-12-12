2391bc548b2cb73bc0e919f23fa602c270b4de2f
bwinklesky <46791574+bwinklesky@users.noreply.github.com>
2025-12-12 09:47:54 -0700

    repo

---
+++ webpack.config.cjs
@@ -0,0 +1,94 @@
+﻿const path = require("path");
+const ArcGISPlugin = require("@arcgis/webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var webpack = require('webpack');
+
+module.exports = {
+    mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+    entry: {
+        main: {
+            import: path.join(__dirname, "ClientApp", "main.ts"),
+            library: {
+                name: "pf",
+                type: "var"
+            },
+        }
+    },
+    output: {
+        path: path.resolve(__dirname, "wwwroot/app"),
+        filename: "[name].js",
+        //filename: "[name].[chunkhash].js",
+        publicPath: "/_content/PrecisionFarms/app/",
+        libraryTarget: 'var',
+        library: 'pf',
+        assetModuleFilename: 'images/[hash][ext][query]'
+    },
+    resolve: {
+        extensions: [".js", ".ts"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader"
+            },
+
+            //{
+            //    test: /\.s[ac]ss$/i,
+            //    use: [MiniCssExtractPlugin.loader, "css-loader", {
+            //        loader: "sass-loader",
+            //        options: {
+            //            // Prefer `dart-sass`
+            //            implementation: require.resolve("sass"),
+            //            sassOptions: {
+            //                indentWidth: 4,
+            //                includePaths: ["./ClientApp"]
+            //            },
+
+            //        },
+            //    }]
+            //},
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            // For webpack v5
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                // More information here https://webpack.js.org/guides/asset-modules/
+                type: "asset"
+            },
+        ]
+    },
+    //optimization: {
+    //    splitChunks: {
+    //        cacheGroups: {
+    //            vendor: {
+    //                test: /[\\/]node_modules[\\/](kendo-ui-core|bootstrap|jquery)[\\/]/,
+    //                name: 'vendor',
+    //                chunks: 'all',
+    //            }                
+    //        },
+    //    }
+    //},
+    plugins: [
+        new webpack.ProvidePlugin({
+            // $: 'jquery',
+            // jQuery: 'jquery',
+            //kendo: 'kendo-ui-core',
+            'bootstrap':'bootstrap',
+            'window.kendo': 'kendo-ui-core',
+            'window.bootstrap': 'bootstrap'
+        }),
+        new ArcGISPlugin(),
+        new CleanWebpackPlugin(),
+        //new HtmlWebpackPlugin({
+        //    template: "./src/index.html"
+        //}),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+            //filename: "css/[name].[chunkhash].css"
+        })
+    ]
+};
